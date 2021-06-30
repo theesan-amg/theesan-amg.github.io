2d098d8d5f6a60ebb4298747192e2c4aa7186a6e
theesan-amg <81262199+theesan-amg@users.noreply.github.com>
2021-06-30 15:48:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let a = 31
+let b = 12
+let sign = "-"
+
+if (sign === "+") {
+  console.log(a + b);
+}
+if (sign === "-") {
+  console.log(a - b);
+}
+if (sign === "*") {
+  console.log(a * b);
+}
+if (sign === "%") {
+  console.log(a % b);
+} else {
+  console.log("Erreur");
+}
